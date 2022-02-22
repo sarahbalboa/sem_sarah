8412a50de59c69e6f3dfd503f8b43fecbdf802d3
sarahbalboa <40496531@live.napier.ac.uk>
2022-02-22 15:21:05 +0000

    Employee java app

--- src/main/java/com/sarah/sem/App.java
+++ src/main/java/com/sarah/sem/App.java
@@ -33,7 +33,7 @@ public class App
             {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
-                // Connect to database
+                // Connect to database ___________> change to db:3306 localhost:33060
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
@@ -77,9 +77,9 @@ public class App
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
+                    "SELECT emp_no, first_name, last_name  "
+                            + "FROM employees" +
+                             "WHERE emp_no = " + ID;
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
@@ -90,6 +90,10 @@ public class App
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
+                //emp.title = rset.getString("title");
+                //emp.salary = rset.getInt("salary");
+               // emp.dept_name = rset.getString("departments.dept_name");
+               // emp.manager = rset.getString("dept_manager.dept_no");
                 return emp;
             }
             else
