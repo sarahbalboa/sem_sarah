d7d4cd316b5761e44b6aac621208ffdbcd6a1588
sarahbalboa <40496531@live.napier.ac.uk>
2022-02-25 00:25:30 +0000

    Add code for printing all the employees salaries (not working)

--- src/main/java/com/sarah/sem/App.java
+++ src/main/java/com/sarah/sem/App.java
@@ -244,6 +244,24 @@ public class App
             return null;
         }
     }
+
+    /**
+     * Prints a list of employees.
+     * @param employees The list of employees to print.
+     */
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
+        }
+    }
     public static void main(String[] args)
     {
         // Create new Application
@@ -274,6 +292,8 @@ public class App
 
         // Test the size of the returned data - should be 240124
         System.out.println(employees.size());
+        a.printSalaries(employees);
+
 
         // Disconnect from database
         a.disconnect();
