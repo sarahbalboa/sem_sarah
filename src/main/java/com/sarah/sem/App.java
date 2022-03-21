a5cfb2b87de0efba3db0d2c74cf0736a2643ffce
sarahbalboa <40496531@live.napier.ac.uk>
2022-03-21 23:16:48 +0000

    UnitTest integration

--- src/main/java/com/sarah/sem/App.java
+++ src/main/java/com/sarah/sem/App.java
@@ -12,7 +12,7 @@ public class App {
     /**
      * Connect to the MySQL database.
      */
-    public void connect() {
+    public void connect(String location, int delay) {
         try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -26,20 +26,21 @@ public class App {
             System.out.println("Connecting to database...");
             try {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database ------------------ localhost:33060  ------ db:3306
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                Thread.sleep(delay);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://" + location
+                                + "/employees?allowPublicKeyRetrieval=true&useSSL=false",
+                        "root", "example");
                 System.out.println("Successfully connected");
                 break;
             } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " +    Integer.toString(i));
                 System.out.println(sqle.getMessage());
             } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
     }
-
     /**
      * Disconnect from the MySQL database.
      */
@@ -286,7 +287,7 @@ public class App {
         // Create new Application
         App a = new App();
 
-        // Connect to database
+        /* Connect to database
         a.connect();
 
         // ------------------- Issue #6 -----------
@@ -318,6 +319,18 @@ public class App {
         a.printSalaries(employees);
 
         */
+        //New connect ---------------------------
+        if(args.length < 1){
+            a.connect("localhost:33060", 30000);
+        }else{
+            a.connect(args[0], Integer.parseInt(args[1]));
+        }
+
+        // Get Employee
+        String date = "9999-01-01";
+        Employee emp = a.getEmployee(255530, date);
+        // Display results
+        a.displayEmployee(emp);
         // Disconnect from database
         a.disconnect();
     }
